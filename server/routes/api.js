2240ef0d32e09f14ba97289a7f4cd33ccf72d57b
jungyoseok <yasu3237@gmail.com>
2015-12-29 02:59:44 -0800

    restore

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,27 +1,27 @@
-var fs = require('fs');
+// var fs = require('fs');
 
 var Customer = require('../models/customer');
 module.exports = function(router, passport){
 
     ///
-    router.use(function(req, res, next){
-        fs.appendFile('logs.txt', req.path + " token: " + req.query.access_token + "\n",
-            function(err){
-                next();
-            });
-    });
+    // router.use(function(req, res, next){
+    //     fs.appendFile('logs.txt', req.path + " token: " + req.query.access_token + "\n",
+    //         function(err){
+    //             next();
+    //         });
+    // });
 
-    router.get('/testAPI', function(req, res, next){
-        if(req.query.access_token) next();
-        else next('route');
-    },passport.authenticate('bearer', { session: false }),
-        function(req, res){
-        res.json({ SecretData: 'abc123', Authenticated: true });
-    });
+    // router.get('/testAPI', function(req, res, next){
+    //     if(req.query.access_token) next();
+    //     else next('route');
+    // },passport.authenticate('bearer', { session: false }),
+    //     function(req, res){
+    //     res.json({ SecretData: 'abc123', Authenticated: true });
+    // });
 
-    router.get('/testAPI', function(req, res){
-        res.json({ SecretData: 'abc123', Authenticated: false });
-    });
+    // router.get('/testAPI', function(req, res){
+    //     res.json({ SecretData: 'abc123', Authenticated: false });
+    // });
 
     ///
     router.post('/customers', function(req, res){
